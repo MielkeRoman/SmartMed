9dd19643c32fdde4daeb24451451b3027ddd9927
MielkeRoman <MielkeRoman@users.github.com>
2020-12-09 21:10:33 +0300

    Fifth Commit
    Created a processing cycle for symbol apostrophe-like.
    
    Developed lists finded_soft and wiki.
    
    In list_finded would be to write values which meet value of variable text in list soft.
    
    In list wiki would write party web-address which would be to meet list of values finded soft with index wikies.
    
    Such clean memory, 'zeroing' lists soft, finded_soft, wikies.

--- analysis.py
+++ analysis.py
@@ -8,46 +8,38 @@ def products(text):
               "%d0%b7%d0%b4%d0%be%d1%80%d0%be%d0%b2%d1%8c%d0%b5-%d0%bd%d0%b0%d1%86%d0%b8%d0%b8/",
               "%d0%b7%d1%83%d0%b1%d0%bd%d0%b0%d1%8f-%d1%84%d0%b5%d1%8f/", "ehealth_mis/"]
     soft = []
-    x = 0
     i = 0
-    y = 0
     for wiki in wikies:
         z = ('http://kb.royal.co.ua/tag/' + wiki)
         w = req.get(z)
         info = BeautifulSoup(w.text, 'lxml')
         tag = info.title.string.split('—')[0]
-        soft.append({'tag': tag, 'len': len(list(set(text) & set(tag.lower().split())))})
+        i = i + 1
+        soft.append({'tag': tag, 'len': len(list(set(text) & set(tag.lower().split()))), 'i': i})
         print(soft)
 
-        x_max = max(map(lambda obj: obj['len'], soft))
-        print(x_max)
-        for obj in soft:
-            if obj['len'] == x_max:
-                name_product = (obj['tag'])
-                print('Програмний продукт: ', name_product)
+    x_max = max(map(lambda obj: obj['len'], soft))
+    print(x_max)
+    finded_soft = []
+    for obj in soft:
+        if obj['len'] == x_max:
+            finded_soft.append({'tag': obj['tag'], 'i': obj['i']})
 
-        isEqual = all([obj['len'] == x_max for obj in soft])
+    print(finded_soft)
 
-        if isEqual == True:
-            soft = {}
-            i = 0
-            print('Нажаль не вдалося знайти відповідний продукт')
-            for wiki in wikies:
-                i = i + 1
-                z = ('http://kb.royal.co.ua/tag/' + wiki)
-                w = req.get(z)
-                info = BeautifulSoup(w.text, 'lxml')
-                tag = info.title.string.split('—')[0]
-                soft[i] = tag
-            for item in soft.items():
-                print(item)
-            x = int(input(
-                "Якщо Ваше запитання відноситься до роботи одного з вище вказаних ПЗ, оберіть відповідне йому значення! \n"
-                "Якщо НІ - натиніть просто Enter: "))
-            print(x)
-            x -= 1
-            i = 0
-            print(soft)
+    if len(finded_soft) == 0:
+        pass
+    wiki_index = 0
+    wiki = []
+    for obj in finded_soft:
+        print(obj['i'])
+        wiki_index = int(obj['i']) - 1
+        wiki.append(wikies[wiki_index])
+    soft.clear()
+    finded_soft.clear()
+    wikies.clear()
+    print(wiki)
+
 
 
 
@@ -64,6 +56,10 @@ def main():
         g = "Error File!!!"
         print(g)
         text = input("Введіть Ваше запитання або помилку на екрані: ")
+    finally:
+        chars = "`'"
+        for char in chars:
+            text = text.replace(char, '’')
 
     products(text)
 
